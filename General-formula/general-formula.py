997a25801134516dcb09038cd1d4bf66ff432587
absanyal <absanyal.ABS@gmail.com>
2018-03-10 19:42:46 +0530

    added external gitignore

--- General-formula/general-formula.py
+++ General-formula/general-formula.py
@@ -8,11 +8,17 @@ Created on Mon Nov  6 15:46:02 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 2 #No of sites to be evaluated
+N = 100 #No of sites to be evaluated
 
-eta = 0.1
+eta = 0.05
 epsilon = np.zeros(N)
 t = 1
+
+#for i in range(N):
+#    if ((i % 50) == 0 and i>0):
+#        epsilon[i] = 10
+
+epsilon[5] = 0.5
 
 def z(omega):
     return complex(omega, eta)
@@ -24,17 +30,17 @@ def Gcpp(p, omega):
         return 1 / ( z(omega) - epsilon[p - 1] - \
         t * Gcpp(p + 1, omega) * t )
 
-omega_list = np.linspace(-8, 8, num = 1000)
+omega_list = np.linspace(-4, 4, num = 1000)
 
 A_list = []
 
 for omega in omega_list:
     A_list.append( (-1/np.pi) * np.imag( Gcpp(0, omega) ) )
-    
+
 
 
 
 plt.plot(omega_list, A_list)
 plt.title(str(N) + ' sites with formula')
-plt.savefig(str(N)+'sites_formula_full.pdf')
+#plt.savefig(str(N)+'sites_formula_full.pdf')
 plt.show()
--- General-formula/n_site_matrix-no-pbc.py
+++ General-formula/n_site_matrix-no-pbc.py
@@ -8,16 +8,17 @@ Created on Mon Nov  6 15:46:02 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 2 #number of sites
-eta = 0.1
+n = 100 #number of sites
+eta = 0.05
 epsilon = np.zeros(n)
+epsilon[50] = 10
 
 def z(omega):
     return complex(omega, eta)
 
 t = 1.0
 
-omega_list = np.linspace(-8, 8, num = 1000)
+omega_list = np.linspace(-4, 4, num = 1000)
 
 A_mat = []
 for omega in omega_list:
@@ -28,13 +29,14 @@ for omega in omega_list:
         H[i][i + 1] = t
     for i in range(1, n):
         H[i][i-1] = t
-    H[0][n - 1] = 0
-    H[n - 1][0] = 0
+#    H[0][n - 1] = 0
+#    H[n - 1][0] = 0
     Hi = np.linalg.inv(H)
     A_mat.append( -1/(np.pi) * np.imag(Hi[0][0]) )
+    #A_mat.append( -1/(np.pi) * np.imag(np.trace(Hi)) / n )
 
 plt.plot(omega_list, A_mat)
 plt.title(str(n) + ' site matrix inverted directly with no PBC')
-plt.savefig(str(n)+'sitematrixinvert-no-pbc.pdf')
+#plt.savefig(str(n)+'sitematrixinvert-no-pbc.pdf')
 #plt.savefig(str(n)+'sitematrixinvert-with-pbc.pdf')
 plt.show()
