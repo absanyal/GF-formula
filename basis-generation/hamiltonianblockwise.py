3c2f7ae893728e64c7b0bc7493c64c5474b6dff3
absanyal <absanyal.ABS@gmail.com>
2018-03-31 04:08:32 +0530

    Generate the overlap matrix between any two Fock space blocks

---
+++ basis-generation/hamiltonianblockwise.py
@@ -0,0 +1,79 @@
+###### Sat Mar 31 03:35:13 IST 2018
+
+import basisgeneration as bg
+import progbar as pb
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+os.system('cls')
+os.system('clear')
+
+N = 4
+n = 4
+Sz = (0.5 * n) % 1
+
+l_n1 = 1
+l_n2 = 2
+
+t = 1
+tprime = 2
+U = 4
+
+basis1 = bg.createlfsbasis(N, n, Sz, l_n1)
+basis2 = bg.createlfsbasis(N, n, Sz, l_n2)
+
+
+def mel(state1, state2):
+
+    #calculate the hopping to right
+    term = 0
+    for sigma in [-1, 1]:
+        for i in range(N):
+            for j in range(N):
+                if (i != j and abs(i - j) == 1):
+                    s2 = bg.clonestate(state2)
+                    s2.move(i, j, sigma)
+                    termtemp = bg.innerproduct(state1, s2)
+                    #print(termtemp)
+                    term += termtemp
+#                    print((i, j, sigma), state1.getstate(), state2.getstate(),
+#                          s2.getstate(), termtemp, sep = '\t')
+#        print("*" * 80)
+
+    return term
+
+
+H = np.zeros((len(basis1), len(basis2)), dtype=np.float)
+
+for bi in range(len(basis1)):
+    for bj in range(len(basis2)):
+
+        state1 = basis1[bi]
+        state2 = basis2[bj]
+
+        if (state1.getleftnum() == state2.getleftnum()):
+            H[bi][bj] = t * mel(state1, state2)
+            #H[bj][bi] = H[bi][bj]
+        if (state1.getleftnum() != state2.getleftnum()):
+            H[bi][bj] = tprime * mel(state1, state2)
+            #H[bj][bi] = H[bi][bj]
+
+        if ( l_n1 == l_n2 and bi == bj):
+            a = basis1[bi]
+            particles = np.array(a.upconfig) + np.array(a.downconfig)
+            for nump in particles:
+                if (nump == 2):
+                    H[bi][bj] += U
+
+        # Hprog = len(basis1) * (bi + 1) + bj + 1
+        # pb.progressbar(Hprog, 0, len(basis1) * len(basis2))
+
+print('\nThe Hamiltonian matrix is:')
+for i in range(len(basis1)):
+    for j in range(len(basis2)):
+        if (H[i, j] >= 0):
+            print(' ', H[i, j], end=' ', sep='')
+        else:
+            print(H[i, j], end=' ', sep='')
+    print('')
