3c0a946aa3f8585d8729bd9c485caab83ef03ccb
absanyal <absanyal.ABS@gmail.com>
2018-03-30 23:46:30 +0530

    testing the createlbsbasis function

--- basis-generation/testing.py
+++ basis-generation/testing.py
@@ -13,42 +13,19 @@ import itertools
 
 import time
 
-N = 4
-n = 3
+N = 6
+n = 4
 Sz = (0.5 * n) % 1
 
-nu = int(Sz + 0.5 * n)
-nd = int(n - nu)
+l_n = 1
+l_Sz = (0.5 * l_n) % 1
 
-#print(nu, nd)
-
-us = [1 for i in range(nu)] + [0 for i in range(N-nu)]
-ds = [1 for i in range(nd)] + [0 for i in range(N-nd)]
-
-us_list = set(list(itertools.permutations(us)))
-ds_list = set(list(itertools.permutations(ds)))
-
-uobasis = []
-
-for us_i in us_list:
-    for ds_i in ds_list:
-        uobasis.append(bg.state(us_i, ds_i))
-
-basis = []
+b = bg.createlbsbasis(N, n, Sz, l_n, l_Sz)
 
 i = 0
-for n_l in range(n+1)[::-1]:
-    j = 0
-    spins = 0.5 * np.array(list(range(-n_l, n_l + 1)))
-    for Sz_l in spins:
-        tempbasis = bg.createsubbasis(uobasis, n_l, Sz_l)
-        basis += tempbasis
+for s in b:
 
-        for state in tempbasis:
-            print(i, j, (n_l, Sz_l), state.getstate(),\
-            state.intequiv(), sep = '\t')
-            i += 1
-            j += 1
-#        #print(n_l, '\t', Sz_l)
-#
-    print("*" * 80)
+    print(i, s.getstate(), s.intequiv(), (s.getleftnum(),  \
+    s.getleftSz()), sep = '\t')
+
+    i += 1
