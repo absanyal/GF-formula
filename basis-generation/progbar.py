8faa1dfc9ce8dc25022a80b0b1ba80ee4860a98b
absanyal <absanyal.ABS@gmail.com>
2018-04-16 16:50:10 +0530

    updated symbols to retro ascii for crt

--- basis-generation/progbar.py
+++ basis-generation/progbar.py
@@ -7,38 +7,46 @@ Created on Sun Sep 24 20:17:26 2017
 A module form of the progressbar that can be reused anywhere.
 """
 
-#imports
+# imports
 import sys
 from itertools import cycle
 
-symbols = [
-        chr(8942),
-        chr(8944),
-        chr(8943),
-        chr(8945)
-        ]
+# symbols = [
+#         chr(8942),
+#         chr(8944),
+#         chr(8943),
+#         chr(8945)
+#         ]
 
-sym_list = cycle( symbols )
+symbols = [
+    '(  ',
+    ' | ',
+    '  )',
+    ' | '
+]
+
+sym_list = cycle(symbols)
 
 progress_symbol = chr(9608)
 emptysymbol = chr(9617)
 
 stepsize = 5
 
-def progressbar( v, v_min, v_max ):
 
-    #percentage completed
-    pc = round( ( v - v_min ) / ( v_max - v_min ) * 100, 1 )
+def progressbar(v, v_min, v_max):
 
-    counter = int( pc / stepsize )
+    # percentage completed
+    pc = round((v - v_min) / (v_max - v_min) * 100, 1)
 
-    symbol = next( sym_list )
+    counter = int(pc / stepsize)
+
+    symbol = next(sym_list)
 
     message = "Progress: " + symbol + "  " + progress_symbol * counter \
-    + emptysymbol * int( ( 100 / stepsize) - counter ) + " " + str( pc ) + " %"
-    sys.stdout.write( '\r' + message )
+        + emptysymbol * int((100 / stepsize) - counter) + " " + str(pc) + " %"
+    sys.stdout.write('\r' + message)
 
-    if ( v >= v_max ):
+    if (v >= v_max):
         done = "Progress: Done!"
-        sys.stdout.write( '\r' + " " * len( message ) )
-        sys.stdout.write( '\r' + done )
+        sys.stdout.write('\r' + " " * len(message))
+        sys.stdout.write('\r' + done)
