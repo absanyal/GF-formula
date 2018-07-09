32bff09a31386d05add30e976bc0f7a3d7826c20
absanyal <absanyal.ABS@gmail.com>
2018-07-09 10:06:15 +0530

    set default value of onsite energy to 0

--- fortran/sp_rgf_pbc/constants.f95
+++ fortran/sp_rgf_pbc/constants.f95
@@ -5,7 +5,7 @@ module constants
     complex, parameter :: i = (0, 1)                    ! imaginary i
     real, parameter :: pi = 4 * atan(1.0)
     
-    real, parameter :: epsilon0 = 1                     ! on-site energy
+    real, parameter :: epsilon0 = 0                     ! on-site energy
     real, parameter :: eta = 0.1                        ! regulator
     real, parameter :: t = 1.0                          ! hopping constant
     integer, parameter :: numpts = 10000                !number of points
