29e22d37a46c2bd8925694b6a1d9d9415f5d85ac
absanyal <absanyal.ABS@gmail.com>
2018-08-22 17:01:39 +0530

    further optimization and full basis generation

--- basis-generation/prototype_sectorgen.py
+++ basis-generation/prototype_sectorgen.py
@@ -14,10 +14,12 @@ from math import factorial
 os.system('cls')
 os.system('clear')
 
+dm = 0
+
 seed = [[0], [1]]
 
-N = 20
-n = 10
+N = 16
+n = 8
 
 oldb = seed[:]
 newb = []
@@ -25,15 +27,22 @@ newb = []
 # print(oldb)
 
 expectedbasissize = factorial(N) / (factorial(N - n) * factorial(n))
-print("Basis size should be", expectedbasissize)
+print("Calculating for", N, "sites with", n, "particles")
+print("Sector size should be", expectedbasissize)
 
 finalbasis = []
+
+t1 = time.perf_counter()
 
 counter = 0
 while (len(finalbasis) <= expectedbasissize and len(oldb) > 0):
     newb = []
     for state in oldb:
-        # print("Looking at", state)
+        if (dm == 1):
+            print('-' * 50)
+            print("Looking at", state)
+            time.sleep(1)
+        counter += 1
         no_of_p_assigned = sum(state)
         no_of_sites_assigned = len(state)
         no_of_p_unassigned = n - sum(state)
@@ -43,44 +52,63 @@ while (len(finalbasis) <= expectedbasissize and len(oldb) > 0):
             finalbasis.append(state)
             # todelete = state[:]
             # oldb.remove(state)
-            # print("Adding to basis", state)
+            if (dm == 1):
+                print("Adding to basis", state)
         elif (no_of_sites_unassigned < no_of_p_unassigned):
             # todelete = state[:]
             # oldb.remove(state)
-            # print("Deleting", state)
+            if (dm == 1):
+                print("Skipping", state)
             continue
+        elif (no_of_p_unassigned == no_of_sites_unassigned):
+            state += [1] * int(no_of_sites_unassigned)
+            # newb.append(state)
+            finalbasis.append(state)
+            if (dm == 1):
+                print("Padding with ones and adding to final", state)
         elif (no_of_p_unassigned == 0 and no_of_sites_unassigned > 0):
             state += [0] * int(no_of_sites_unassigned)
-            newb.append(state)
-            # print("Padding with zeros", state)
+            # newb.append(state)
+            finalbasis.append(state)
+            if (dm == 1):
+                print("Padding with zeros and adding to final", state)
         else:
             temps0 = state + [0]
             temps1 = state + [1]
             newb.append(temps0)
             newb.append(temps1)
-            # print("Branching", state)
-        # print("Temp basis is now at\n", oldb)
-        # print('-' * 50)
-        # time.sleep(1)
-        counter += 1
+            if (dm == 1):
+                print("Branching", state)
+                print("Created", temps0, "and", temps1)
 
     oldb = newb[:]
     # print("#" * 50)
-    # time.sleep(1)
-    # print(oldb)
-    if (oldb != []):
-        p = len(oldb[0])
-    pb.progressbar(p, 0, N)
+    # print("Basis is now at", oldb)
+    # print("#" * 50)
+    # if (oldb != []):
+    #     p = len(oldb[0])
+    # pb.progressbar(p, 0, N)
 
-# print("Final length:", len(finalbasis))
+t2 = time.perf_counter()
 
-# if (len(finalbasis) == expectedbasissize):
-#     print("SIZE MATCH")
-# else:
-#     print("SIZE MISMATCH")
+print("Finished calculations in", round(t2 - t1, 3), "seconds.")
+
+print("Final length:", len(finalbasis))
+
+if (len(finalbasis) == expectedbasissize):
+    print("SIZE MATCH")
+else:
+    print("SIZE MISMATCH")
 
 print("Total number of steps required:", counter, "to generate",
-      expectedbasissize, "elements, efficiency = ",
-      round(expectedbasissize / counter, 3))
+      expectedbasissize, "elements, overcounting factor = ",
+      round(counter / expectedbasissize, 3))
 
-# print("Final basis is\n", finalbasis)
+fullbasis = []
+
+i = 0
+for s1 in finalbasis:
+    for s2 in finalbasis:
+        s = bg.state(s1, s2)
+        print(i, s.getstate())
+        i += 1
