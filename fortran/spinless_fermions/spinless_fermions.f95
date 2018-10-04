75cd5cbe03242e1e872f8b54a0963d174ec55817
absanyal <absanyal.ABS@gmail.com>
2018-10-04 12:51:13 +0530

    implementing main stuff

--- fortran/spinless_fermions/spinless_fermions.f95
+++ fortran/spinless_fermions/spinless_fermions.f95
@@ -5,14 +5,27 @@ program spinless_fermions
 
     integer :: num_sites
     integer :: num_particles
-    integer :: sigma
+    integer :: smax
+    integer :: ns
+
+    integer :: sigma, ns1, ns2, alpha1, alpha2, beta1, beta2
 
     type (state) :: s1
-    type (state) :: s2
+    type (state) :: ss1
+    type (state) :: ss2
+    type (state) :: ss3
 
-    call setstate(s1, 3, 2, 1, 0)
-    call setstate(s2, 3, 1, 0, 1)
+    smax = num_sites - 1
 
-    print *, mel(s1, s2)
+    ns = num_particles
+
+     call setstate(s1, 3, 2, 1, 0)
+     print *, getstate(s1)
+     ss1 = relegate0(s1)
+     print *, getstate(ss1)
+     ss2 = relegate1(s1)
+     print *, getstate(ss2)
+     ss3 = relegate0(ss1)
+     print *, getstate(ss3)
 
 end program spinless_fermions
--- fortran/spinless_fermions/statemanip.f95
+++ fortran/spinless_fermions/statemanip.f95
@@ -3,10 +3,10 @@ module statemanip
 implicit none
 
     type state
-        integer :: s
-        integer :: n_s
-        integer :: alpha_s
-        integer :: beta_s
+        integer :: s = 0
+        integer :: n_s = 0
+        integer :: alpha_s = 0
+        integer :: beta_s = 0
     end type state
 
 contains
@@ -34,23 +34,24 @@ contains
         astate%beta_s = vbeta_s
     end subroutine setstate
 
-    subroutine getstate(astate)
+    function getstate(astate)
         type(state), intent(inout) :: astate
-        integer, dimension(4) :: output
-        output(1) = astate%s
-        output(2) = astate%n_s
-        output(3) = astate%alpha_s
-        output(4) = astate%beta_s
-        write (*,*) output
-    end subroutine getstate
+        integer, dimension(4) :: getstate
+        getstate(1) = astate%s
+        getstate(2) = astate%n_s
+        getstate(3) = astate%alpha_s
+        getstate(4) = astate%beta_s
+    end function getstate
 
-    recursive real function mel(s1, s2) result(fullmel)
+    function mel(s1, s2)
         implicit none
 
         type(state), intent(inout) :: s1
         type(state), intent(inout) :: s2
 
         real :: Hu, Hl, Hs, Hsp1, Hsm1
+
+        real :: mel
 
         integer :: vns1, valphas1, vbetas1
         integer :: vns2, valphas2, vbetas2
@@ -63,21 +64,83 @@ contains
         valphas2 = s2%alpha_s
         vbetas2 = s2%beta_s
 
-        Hsp1 = kdelta(vns1, vns2) * kdelta(valphas1, valphas2) * &
-                (kdelta(vbetas1, vbetas2 + 1) &
-                + kdelta(vbetas1, vbetas2 - 1))
-        
-        Hsm1 = kdelta(vns1, vns2) * kdelta(vbetas1, vbetas2) * &
-                (kdelta(valphas1, valphas2 + 1) &
-                + kdelta(valphas1, valphas2 - 1))
-        
-        Hs = kdelta(vns1, vns2-1) * kdelta(valphas1, valphas2-1) * &
-                kdelta(vbetas1, vbetas2 + 1) + &
-            kdelta(vns1, vns2+1) * kdelta(valphas1, valphas2+1) * &
-                kdelta(vbetas1, vbetas2 - 1)
-        
-        fullmel = Hs + Hsp1 + Hsm1
+        if (s1%s .eq. s2%s) then
+            if (vns1 .ne. vns2 .or. &
+                valphas1 .ne. valphas2 .or. &
+                vbetas1 .ne. vbetas2) then
+                Hsp1 = kdelta(vns1, vns2) * kdelta(valphas1, valphas2) * &
+                        (kdelta(vbetas1, vbetas2 + 1) &
+                        + kdelta(vbetas1, vbetas2 - 1))
+                
+                Hsm1 = kdelta(vns1, vns2) * kdelta(vbetas1, vbetas2) * &
+                        (kdelta(valphas1, valphas2 + 1) &
+                        + kdelta(valphas1, valphas2 - 1))
+                
+                Hs = kdelta(vns1, vns2-1) * kdelta(valphas1, valphas2-1) * &
+                        kdelta(vbetas1, vbetas2 + 1) + &
+                    kdelta(vns1, vns2+1) * kdelta(valphas1, valphas2+1) * &
+                        kdelta(vbetas1, vbetas2 - 1)
+            else
+                Hs = 0
+                Hsp1 = 0
+                Hsm1 = 0
+            end if
+
+            if (vns1 .eq. vns2 .and. &
+                valphas1 .eq. valphas2 .and. &
+                vbetas1 .eq. vbetas2 .and. s1%s .gt. 2) then
+                Hl = 1
+            else
+                Hl = 0
+            end if
+            
+            mel = Hs + Hsp1 + Hsm1 + Hl
+        else
+            mel = 0
+        end if
 
     end function mel
+
+    function checkvalidity(s1)
+        type(state), intent(inout) :: s1
+        logical :: checkvalidity
+        if (s1%s .eq. 0 .and. s1%n_s .eq. 0 .and. &
+            s1%alpha_s .eq. 0 .and. s1%beta .eq. 0) then
+            checkvalidity = .false.
+        end if
+        if (s1%s .ge. 2 .and. s1%n_s .ge. 0) then
+            checkvalidity = .true.
+        else
+            checkvalidity = .false.
+        end if
+    end function
+
+    function relegate0(s1)
+        type(state), intent(inout) :: s1
+        type(state) :: relegate0
+
+        if (s1%n_s - (0+s1%alpha_s) .ge. 0) then
+            call setstate(relegate0, &
+                (s1%s)-1, s1%n_s - (0+s1%alpha_s), 0, s1%alpha_s)
+        end if
+    end function
+
+    function relegate1(s1)
+        type(state), intent(inout) :: s1
+        type(state) :: relegate1
+        if (s1%n_s - (1+s1%alpha_s) .ge. 1) then
+            call setstate(relegate1, &
+                (s1%s)-1, s1%n_s - (1+s1%alpha_s), 1, s1%alpha_s)
+        end if
+    
+    end function
+
+    ! subroutine findmels
+    !     type(state), intent(inout) :: s1
+    !     type(state), intent(inout) :: s2
+
+
+        
+    ! end subroutine findmels
 
 end module statemanip
