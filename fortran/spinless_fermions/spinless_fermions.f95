85120d501b823fc7666cc989247277bb0a820882
absanyal <absanyal.ABS@gmail.com>
2018-10-05 12:31:55 +0530

    generalized

--- fortran/spinless_fermions/spinless_fermions.f95
+++ fortran/spinless_fermions/spinless_fermions.f95
@@ -3,29 +3,25 @@ program spinless_fermions
     use statemanip
     implicit none
 
-    integer :: num_sites
-    integer :: num_particles
+    integer :: num_sites = 4
+    integer :: num_particles = 2
+
     integer :: smax
     integer :: ns
-
-    integer :: sigma, ns1, ns2, alpha1, alpha2, beta1, beta2
 
     type (state) :: s1
     type (state) :: s2
     type (state) :: s3
     type (state) :: s4
-    type (state) :: ss1
-    type (state) :: ss2
-    type (state) :: ss3
 
     smax = num_sites - 1
 
     ns = num_particles
 
-    call setstate(s1, 3, 2, 0, 0)
-    call setstate(s2, 3, 2, 1, 0)
-    call setstate(s3, 3, 1, 0, 1)
-    call setstate(s4, 3, 1, 1, 1)
+    call setstate(s1, smax, ns, 0, 0)
+    call setstate(s2, smax, ns, 1, 0)
+    call setstate(s3, smax, ns-1, 0, 1)
+    call setstate(s4, smax, ns-1, 1, 1)
     ! print *, getlstate(s1)
     ! print *, getlstate(s2)
     ! ss1 = relegate0(s1)
@@ -37,6 +33,19 @@ program spinless_fermions
 
     ! print *, checkvalidity(s1)
 
+    call findmels(s1, s1)
+    write (*,*) "**************************"
+    call findmels(s1, s2)
+    write (*,*) "**************************"
+    call findmels(s2, s2)
+    write (*,*) "**************************"
     call findmels(s2, s3)
+    write (*,*) "**************************"
+    call findmels(s3, s3)
+    write (*,*) "**************************"
+    call findmels(s3, s4)
+    write (*,*) "**************************"
+    call findmels(s4, s4)
+    write (*,*) "**************************"
 
 end program spinless_fermions
