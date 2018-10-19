93a876f69cedc965dd33811c993542640e48e581
absanyal <absanyal.ABS@gmail.com>
2018-10-19 12:32:50 +0530

    something is still wrong

--- fortran/spinless_fermions/gfcalc.py
+++ fortran/spinless_fermions/gfcalc.py
@@ -23,17 +23,17 @@ w_list = np.linspace(-5, 5, 1000)
 # w_list = [0]
 A = []
 
-H_3a2 = [0]
+H_3a2 = [[0]]
 H_3c2 = [
     [0, 1],
     [1, 0]]
 H_3b1 = [
     [0, 1],
     [1, 0]]
-H_3d1 = [0]
+H_3d1 = [[0]]
 tau_3a2_3c2 = [[1, 0]]
 tau_3c2_3a2 = transpose(tau_3a2_3c2)
-tau_3b1_3d1 = [[1], [0]]
+tau_3b1_3d1 = transpose(tau_3a2_3c2)
 tau_3d1_3b1 = transpose(tau_3b1_3d1)
 tau_ac_bd = [
     [0, 0, 0],
@@ -43,86 +43,88 @@ tau_bd_ac = transpose(tau_ac_bd)
 
 # print(tau_bd_ac)
 
-H = [
-    [0, 1, 0, ],
-    [1, 0, 1, ],
-    [0, 1, 0, ],
-]
+sH = np.array([
+    [0, 1, 0 ],
+    [1, 0, 1 ],
+    [0, 1, 0 ],
+])
+
+H = np.bmat([
+    [sH, tau_ac_bd],
+    [tau_bd_ac, sH]
+])
+
+# for w in w_list:
+#     fG = G(H, w)
+#     snf = 1 / len(H)
+#     A.append((-snf / pi) * np.imag(np.trace(fG)))
 
 for w in w_list:
-    fG = G(H, w)
-    snf = 1 / len(H)
+    fG = G(sH, w)
+    snf = 1 / len(sH)
     A.append((-snf / pi) * np.imag(np.trace(fG)))
 
 plt.plot(w_list, A)
 
-# print(imag(fG))
+print(np.round(fG, 3))
 
 A = []
 
-# for w in w_list:
+for w in w_list:
 
-#     # print(w)
+    # print(w)
 
-#     g_3a2 = G(H_3a2, w)
-#     g_3c2 = G(H_3c2, w)
+    g_3a2 = G(H_3a2, w)
+    g_3c2 = G(H_3c2, w)
 
-#     lG_3a2 = g_3a2
-#     lG_3c2 = inv(inv(g_3c2) - tmm(tau_3c2_3a2, lG_3a2, tau_3a2_3c2))
+    fG_3a2 = inv(inv(g_3a2) - tmm(tau_3a2_3c2, g_3c2, tau_3c2_3a2))
+    fG_3c2 = inv(inv(g_3c2) - tmm(tau_3c2_3a2, g_3a2, tau_3a2_3c2))
 
-#     rG_3c2 = g_3c2
-#     rG_3a2 = inv(inv(g_3a2) - tmm(tau_3a2_3c2, lG_3c2, tau_3c2_3a2))
+    fG_tau_3a2_3c2 = tmm(fG_3a2, tau_3a2_3c2, g_3c2)
+    # fG_tau_3c2_3a2 = tmm(g_3c2, tau_3c2_3a2, fG_3a2)
 
-#     fG_3a2 = inv(inv(g_3a2) - tmm(tau_3a2_3c2, rG_3c2, tau_3c2_3a2))
-#     fG_3c2 = inv(inv(g_3c2) - tmm(tau_3c2_3a2, lG_3a2, tau_3a2_3c2))
+    fG_tau_3c2_3a2 = transpose(fG_tau_3a2_3c2)
 
-#     fG_tau_3a2_3c2 = tmm(fG_3a2, tau_3a2_3c2, rG_3c2)
-#     fG_tau_3c2_3a2 = tmm(rG_3c2, tau_3c2_3a2, fG_3a2)
+    # print( fG_tau_3a2_3c2 == transpose(fG_tau_3c2_3a2) )
 
-#     # print( fG_tau_3a2_3c2 == transpose(fG_tau_3c2_3a2) )
+    fG_ac = np.bmat(
+        [[fG_3a2, fG_tau_3a2_3c2],
+         [fG_tau_3c2_3a2, fG_3c2]]
+    )
 
-#     fG_ac = np.block(
-#         [[fG_3a2, fG_tau_3a2_3c2],
-#          [fG_tau_3c2_3a2, fG_3c2]]
-#     )
+    g_3b1 = G(H_3b1, w)
+    g_3d1 = G(H_3d1, w)
 
-#     g_3b1 = G(H_3b1, w)
-#     g_3d1 = G(H_3d1, w)
+    fG_3b1 = inv(inv(g_3b1) - tmm(tau_3b1_3d1, g_3d1, tau_3d1_3b1))
+    fG_3d1 = inv(inv(g_3d1) - tmm(tau_3d1_3b1, g_3b1, tau_3b1_3d1))
 
-#     lG_3b1 = g_3b1
-#     lG_3d1 = inv(inv(g_3d1) - tmm(tau_3d1_3b1, lG_3b1, tau_3b1_3d1))
+    fG_tau_3b1_3d1 = tmm(fG_3b1, tau_3b1_3d1, g_3d1)
+    fG_tau_3d1_3b1 = tmm(g_3d1, tau_3d1_3b1, fG_3b1)
 
-#     rG_3d1 = g_3d1
-#     rG_3b1 = inv(inv(g_3b1) - tmm(tau_3b1_3d1, lG_3d1, tau_3d1_3b1))
+    # print( fG_tau_3b1_3d1 == transpose(fG_tau_3d1_3b1) )
 
-#     fG_3b1 = inv(inv(g_3b1) - tmm(tau_3b1_3d1, rG_3d1, tau_3d1_3b1))
-#     fG_3d1 = inv(inv(g_3d1) - tmm(tau_3d1_3b1, lG_3b1, tau_3b1_3d1))
+    fG_bd = np.bmat(
+        [[fG_3b1, fG_tau_3b1_3d1],
+         [fG_tau_3d1_3b1, fG_3d1]]
+    )
 
-#     fG_tau_3b1_3d1 = tmm(lG_3b1, tau_3b1_3d1, fG_3d1)
-#     fG_tau_3d1_3b1 = tmm(fG_3d1, tau_3d1_3b1, lG_3b1)
+    g_ac = fG_ac
+    g_bd = fG_bd
 
-#     # print( fG_tau_3b1_3d1 == transpose(fG_tau_3d1_3b1) )
+    lG_ac = g_ac
+    lG_bd = inv(inv(g_bd) - tmm(tau_bd_ac, lG_ac, tau_ac_bd))
 
-#     fG_bd = np.block(
-#         [[fG_3b1, fG_tau_3b1_3d1],
-#          [fG_tau_3d1_3b1, fG_3d1]]
-#     )
+    rG_bd = g_bd
+    rG_ac = inv(inv(g_ac) - tmm(tau_ac_bd, lG_bd, tau_bd_ac))
 
-#     g_ac = fG_ac
-#     g_bd = fG_bd
+    # fG_ac = inv(inv(g_ac) - tmm(tau_ac_bd, rG_bd, tau_bd_ac))
+    # fG_bd = inv(inv(g_bd) - tmm(tau_bd_ac, lG_ac, tau_ac_bd))
 
-#     lG_ac = g_ac
-#     lG_bd = inv(inv(g_bd) - tmm(tau_bd_ac, lG_ac, tau_ac_bd))
+    # snf = 1 / (len(fG_ac) + len(fG_bd))
+    # A.append((-snf / pi) * np.imag(np.trace(fG_ac) + np.trace(fG_bd)))
 
-#     rG_bd = g_bd
-#     rG_ac = inv(inv(g_ac) - tmm(tau_ac_bd, lG_bd, tau_bd_ac))
-
-#     fG_ac = inv(inv(g_ac) - tmm(tau_ac_bd, rG_bd, tau_bd_ac))
-#     fG_bd = inv(inv(g_bd) - tmm(tau_bd_ac, lG_ac, tau_ac_bd))
-
-#     snf = 1 / (len(fG_ac) + len(fG_bd))
-
-#     A.append((-snf / pi) * np.imag(np.trace(fG_ac) + np.trace(fG_bd)))
+    snf = 1 / (len(fG_bd))
+    A.append((-snf / pi) * np.imag(np.trace(fG_bd)))
 
 # f = open('dos_smart.dat', 'w')
 
@@ -131,41 +133,47 @@ A = []
 
 # f.close()
 
-# plt.plot(w_list, A)
+plt.plot(w_list, A)
 
-# # plt.show()
+print(np.round(fG - fG_ac, 3))
+#
+# plt.show()
 
 # print(fG_ac[1:3, 1:3])
 
-H1 = [
-    [0]
-]
-H2 = [
-    [0, 1],
-    [1, 0]
-]
+# H2 = [
+#     [0]
+# ]
+# H1 = [
+#     [0, 1],
+#     [1, 0]
+# ]
 
-tau12 = [
-    [1, 0],
-]
+# tau21 = [
+#     [1, 0],
+# ]
 
-tau21 = transpose(tau12)
+# tau12 = transpose(tau21)
 
-print(tau21)
+# print(tau21)
 
-for w in w_list:
-    g1 = G(H1, w)
-    g2 = G(H2, w)
 
-    fG1 = inv(inv(g1) - tmm(tau12, g2, tau21))
-    fG2 = inv(inv(g2) - tmm(tau21, g1, tau12))
+# A = []
+# for w in w_list:
+#     g1 = G(H1, w)
+#     g2 = G(H2, w)
 
-    snf = 1/(len(g1) + len(g2))
+#     fG1 = inv(inv(g1) - tmm(tau12, g2, tau21))
+#     fG2 = inv(inv(g2) - tmm(tau21, g1, tau12))
 
-    A.append((-snf/pi) * imag(trace(fG1) + trace(fG2)))
+#     snf = 1/(len(g1) + len(g2))
 
-plt.plot(w_list, A)
+#     A.append((-snf/pi) * imag(trace(fG1) + trace(fG2)))
 
-print(np.dot(tau12, tau21))
+# plt.plot(w_list, A)
 
-plt.show()
+# # # print(np.dot(tau12, tau21))
+
+#plt.show()
+
+# # print(np.round(fG1, 3))
