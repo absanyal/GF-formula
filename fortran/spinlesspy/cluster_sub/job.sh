bba7cb77fb4d56ce619b18f26fa49e7e60a8de1c
absanyal <amit.sanyal@niser.ac.in>
2019-03-25 11:41:59 +0530

    initial commit

---
+++ fortran/spinlesspy/cluster_sub/job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+rm -r w*
+for i in {1..100}
+do
+    WMIN=-5
+    WMAX=45
+    DELTA1=$((WMAX-WMIN))
+    DELTA=$(div $DELTA1 100)
+    W=`echo $WMIN+$DELTA\*$i|bc`
+    # echo $DELTA
+    echo $W
+    mkdir w${i}
+    cd w${i}
+    echo $W > omega_value.dat
+    cp ../spinless.py spinless.py
+    cp ../statemanip.py statemanip.py
+    cp ../job.sh job.sh
+    chmod 777 job.sh
+    # qsub job.sh
+    cd ../
+done
